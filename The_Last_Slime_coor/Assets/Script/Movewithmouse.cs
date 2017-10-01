031f6209a30d55fb9087746bfebd5efd06becc39
unknown <dark_ream@hotmail.com>
2017-10-01 21:31:35 +0900

    Swapping

---
+++ The_Last_Slime_coor/Assets/Script/Movewithmouse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movewithmouse : MonoBehaviour {
+	public float speed = 1.5f;
+	private Vector3 target;
+	private Vector2 target2d;
+	private Rigidbody2D rb2d;
+	void Start () {
+		target = transform.position;
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		if (Input.GetMouseButton(0)) {
+			target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			target.z = transform.position.z;
+			target2d = new Vector2 (target.x, target.y);
+			var dir = target - transform.position;
+			var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis(angle-90, transform.forward); //-90 for face toward mouse
+			transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+			//rb2d.velocity =target2d.normalized * speed;
+		}
+
+
+	}    
+}
