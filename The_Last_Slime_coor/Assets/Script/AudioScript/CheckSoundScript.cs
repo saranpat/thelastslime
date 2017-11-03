ee750659ee5dbd1943ae44f5b05ddfa66f27060d
sapphireVGC <siribhums8534@gmail.com>
2017-11-03 17:29:09 +0700

    Loop Sound Add
    - Add Fire Trap Sound
    - Change new technics
    
    ** NEED FIRE TRAP SOUND

--- The_Last_Slime_coor/Assets/Script/AudioScript/CheckSoundScript.cs
+++ The_Last_Slime_coor/Assets/Script/AudioScript/CheckSoundScript.cs
@@ -3,49 +3,140 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CheckSoundScript : MonoBehaviour {
-    public AudioClip WaterSnd;
-    public Camera maincamera;
+    public Camera MainCam;
 
-    private AudioSource AuS;
+    private AudioSource[] AuS;
+    private GameObject[] waterBlk;
+    private GameObject[] fireBlk;
+    private GameObject closestWaterBlk;
+    private GameObject closestFireBlk;
     private GameObject target;
 
-    private bool waterFound;
+    private bool waterSndPlay;
+    private bool fireSndPlay;
+    private bool camouSndPlay;
+
+    private float waterTiming;
+    private float fireTiming;
+    private float camouTiming;
+    private float waterDelay;
+    private float fireDelay;
+    private float camouDelay;
+    private float range;
 
     // Use this for initialization
     void Start () {
-        AuS = GetComponent<AudioSource>();
+        AuS = GetComponents<AudioSource>();
 
-        waterFound = false;
+        waterSndPlay = false;
+        fireSndPlay = false;
+        camouSndPlay = false;
+
+        waterTiming = Time.time;
+        fireTiming = Time.time;
+        camouTiming = Time.time;
+
+        for (int i = 0; i < AuS.Length; i++)
+        {
+            AuS[i].Stop();
+        }
+
+        waterDelay = AuS[0].clip.length;
+        fireDelay = AuS[1].clip.length;
+        camouDelay = AuS[2].clip.length;
+
+        range = 4.0f;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        target = maincamera.gameObject.GetComponent<CameraScript>().target;
+        target = MainCam.gameObject.GetComponent<CameraScript>().target;
         transform.position = target.transform.position;
 
-        if (waterFound)
-        {
-            waterFound = false;
-            AuS.clip = WaterSnd;
-            AuS.loop = true;
-            AuS.volume = 0.5f;
-            AuS.Play();
-        }
-    }
+        waterBlk = GameObject.FindGameObjectsWithTag("Water");
+        fireBlk = GameObject.FindGameObjectsWithTag("Fire");
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "Water")
+        if (waterBlk != null)
         {
-            waterFound = true;
-        }
-    }
+            for (int i = 0; i < waterBlk.Length; i++)
+            {
+                if (closestWaterBlk == null)
+                    closestWaterBlk = waterBlk[i];
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.tag == "Water")
+                if (Vector2.Distance(transform.position, waterBlk[i].transform.position) <
+                        Vector2.Distance(transform.position, closestWaterBlk.transform.position))
+                    closestWaterBlk = waterBlk[i];
+            }
+        }
+
+        if (fireBlk != null)
         {
-            AuS.Stop();
+            for (int i = 0; i < fireBlk.Length; i++)
+            {
+                if (closestFireBlk == null)
+                    closestFireBlk = fireBlk[i];
+
+                if (Vector2.Distance(transform.position, fireBlk[i].transform.position) <
+                        Vector2.Distance(transform.position, closestFireBlk.transform.position))
+                    closestFireBlk = fireBlk[i];
+            }
+        }
+
+        if (Vector2.Distance(transform.position, closestWaterBlk.transform.position) <= range)
+        {
+            if (waterTiming < Time.time)
+            {
+                waterSndPlay = true;
+                waterTiming = Time.time + waterDelay;
+            }
+        }
+        else
+        {
+            AuS[0].Stop();
+        }
+
+        if (Vector2.Distance(transform.position, closestFireBlk.transform.position) <= range)
+        {
+            if (fireTiming < Time.time)
+            {
+                fireSndPlay = true;
+                fireTiming = Time.time + fireDelay;
+            }
+        }
+        else
+        {
+            AuS[1].Stop();
+        }
+
+        if (Vector2.Distance(transform.position, closestWaterBlk.transform.position) <= 0.75f)
+        {
+            if (camouTiming < Time.time)
+            {
+                camouSndPlay = true;
+                camouTiming = Time.time + camouDelay;
+            }
+        }
+        else
+        {
+            AuS[2].Stop();
+        }
+
+        if (waterSndPlay)
+        {
+            AuS[0].Play();
+            waterSndPlay = false;
+        }
+
+        if (fireSndPlay)
+        {
+            AuS[1].Play();
+            fireSndPlay = false;
+        }
+
+        if (camouSndPlay)
+        {
+            AuS[2].Play();
+            camouSndPlay = false;
         }
     }
 }
