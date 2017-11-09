e49124ca96f4a3d5ff264872fe565223366b17a0
sapphireVGC <siribhums8534@gmail.com>
2017-11-10 00:38:57 +0700

    tap to start
    - blinking text
    - tap to select level scene

--- The_Last_Slime_coor/Assets/Scene/UI/Opening/Scenecontroller.cs
+++ The_Last_Slime_coor/Assets/Scene/UI/Opening/Scenecontroller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class Scenecontroller : MonoBehaviour {
 
@@ -10,21 +11,24 @@ public class Scenecontroller : MonoBehaviour {
 	public Button startbutton2;
 	public Image black;
 	public Animator anim;
+    public Text txt;
 	// Use this for initialization
 	void Start () {
-		startbutton.onClick.AddListener (delegate {
+		/*startbutton.onClick.AddListener (delegate {
 			StartCoroutine(FadingA());
 		});
 		startbutton2.onClick.AddListener (delegate {
 			StartCoroutine(FadingB());
-		});
+		});*/
+
+        StartCoroutine(TextBlink());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-	IEnumerator FadingA()
+    /*IEnumerator FadingA()
 	{
 		anim.SetBool ("Fade", true);
 		yield return new WaitUntil (() => black.color.a == 1);
@@ -35,5 +39,30 @@ public class Scenecontroller : MonoBehaviour {
 		anim.SetBool ("Fade", true);
 		yield return new WaitUntil (() => black.color.a == 1);
 		SceneManager.LoadScene("playable_stage");
-	}
+	}*/
+
+    public void OnTap()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator TextBlink()
+    {
+        while (true)
+        {
+            txt.text = "";
+
+            yield return new WaitForSeconds(0.5f);
+
+            txt.text = "Tap to start!!";
+
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    IEnumerator LoadScene()
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => black.color.a == 1);
+    }
 }
