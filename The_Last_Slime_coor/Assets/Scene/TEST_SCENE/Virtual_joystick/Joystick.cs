73162c6490f3299a4fbedb03aaa5b047360cb4ca
Fune <dark_ream@hotmail.com>
2017-11-11 00:56:26 +0900

    Add Dpad in TEST_SCENE *pls read description*
    Dpad in TEST_SCENE folder
    Dpad is JoystickBG+joystick object in canvas
    Slime move with Move_withjoy Script

---
+++ The_Last_Slime_coor/Assets/Scene/TEST_SCENE/Virtual_joystick/Joystick.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class Joystick : MonoBehaviour,IDragHandler,IPointerUpHandler,IPointerDownHandler {
+
+	private Image bgImg;
+	private Image joyimg;
+	private Vector2 inputvec;
+	private void Start()
+	{
+		bgImg = GetComponent<Image> ();
+		joyimg = transform.GetChild (0).GetComponent<Image> ();
+	}
+	public virtual void OnDrag (PointerEventData ped)
+	{
+		Vector2 pos;
+		if(RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImg.rectTransform,ped.position,ped.pressEventCamera,out pos))
+			{
+			pos.x = (pos.x / bgImg.rectTransform.sizeDelta.x);
+			pos.y = (pos.y / bgImg.rectTransform.sizeDelta.y);
+
+			inputvec = new Vector2 (pos.x * 2 + 1, pos.y * 2 - 1);
+			inputvec = (inputvec.magnitude > 1.0f) ? inputvec.normalized : inputvec;
+
+			//move joystick img 
+			joyimg.rectTransform.anchoredPosition = new Vector2 (inputvec.x * (bgImg.rectTransform.sizeDelta.x/3),inputvec.y*(bgImg.rectTransform.sizeDelta.y/3));
+			Debug.Log (inputvec);
+			}
+	}
+	public virtual void OnPointerDown (PointerEventData ped)
+	{
+		OnDrag (ped);
+	}
+	public virtual void OnPointerUp (PointerEventData ped)
+	{
+		inputvec = Vector2.zero;
+		joyimg.rectTransform.anchoredPosition = Vector2.zero;
+	}
+	public float Horizontal()
+	{
+		if (inputvec.x != 0)
+			return inputvec.x;
+		else
+			return Input.GetAxis ("Horizontal");
+	}
+	public float Vertical()
+	{
+		if (inputvec.y != 0)
+			return inputvec.y;
+		else
+			return Input.GetAxis ("Vertical");
+	}
+}
+
+/*
+target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+target.z = transform.position.z;
+target2d = new Vector2(target.x, target.y);
+
+var dir = target - transform.position;
+var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+transform.rotation = Quaternion.AngleAxis(angle - 90, transform.forward); //-90 for face toward mouse
+transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);*/
---
+++ The_Last_Slime_coor/Assets/Scene/TEST_SCENE/Virtual_joystick/Move_byjoystick.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_byjoystick : MonoBehaviour {
+	public Joystick joy;
+	public float speed = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Vector2 dir = Vector2.zero;
+		float angle;
+		dir.x = joy.Horizontal ();
+		dir.y = joy.Vertical ();
+		Vector3 dirformove = new Vector3 (transform.position.x + dir.x, transform.position.y + dir.y, transform.position.z);
+		angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		//transform.Translate (dir * speed * Time.deltaTime);
+		transform.rotation = Quaternion.AngleAxis(angle-90, transform.forward);
+
+		transform.position = Vector3.MoveTowards(transform.position, dirformove, speed * Time.deltaTime);
+		//transform.rotation = rot;
+
+	}
+}
+/*target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+target.z = transform.position.z;
+target2d = new Vector2(target.x, target.y);
+
+var dir = target - transform.position;
+var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+transform.rotation = Quaternion.AngleAxis(angle - 90, transform.forward); //-90 for face toward mouse
+transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);*/
