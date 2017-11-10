e84814a219a86aa5bdb1bf46862db6121042acce
sapphireVGC <siribhums8534@gmail.com>
2017-11-11 01:41:30 +0700

    Pause menu

--- The_Last_Slime_coor/Assets/Scene/TEST_SCENE/Pause_add/PauseMenuScript.cs
+++ The_Last_Slime_coor/Assets/Scene/TEST_SCENE/Pause_add/PauseMenuScript.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class PauseMenuScript : MonoBehaviour {
-	public Button button;
+	//public Button button;
 	public bool paused;
+
+    private GameObject audio;
+    private GameObject bgm;
+
+    private GameObject pausePanel;
+
 	// Use this for initialization
 	void Start () {
 		paused = false;
+
+        audio = GameObject.Find("CheckSound");
+        bgm = GameObject.Find("SoundManager");
+
+        pausePanel = GameObject.Find("PausePanel");
+
+        pausePanel.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		button.onClick.AddListener (stopgame);
+		//button.onClick.AddListener (stopgame);
 	}
 
 	public void stopgame()
@@ -20,8 +33,14 @@ public class PauseMenuScript : MonoBehaviour {
 		paused = !paused;
 		if (paused) {
 			Time.timeScale = 0;
+            audio.GetComponent<CheckSoundScript>().PauseSound();
+            bgm.GetComponent<SoundManager>().PauseGame();
+            pausePanel.SetActive(true);
 		} else if (!paused) {
 			Time.timeScale = 1;
-		}
+            audio.GetComponent<CheckSoundScript>().UnPauseSound();
+            bgm.GetComponent<SoundManager>().PauseGame();
+            pausePanel.SetActive(false);
+        }
 	}
 }
---
+++ The_Last_Slime_coor/Assets/Script/PausePanelScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PausePanelScript : MonoBehaviour {
+    private string sceneName;
+
+    private GameObject fade;
+
+    // Use this for initialization
+    void Start () {
+		sceneName = SceneManager.GetActiveScene().name;
+
+        fade = GameObject.Find("Fade");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Restart ()
+    {
+        StartCoroutine(loadScene(sceneName));
+    }
+
+    public void Title()
+    {
+        StartCoroutine(loadScene("Open_page"));
+    }
+
+    public void Option()
+    {
+
+    }
+
+    IEnumerator loadScene(string s)
+    {
+        Time.timeScale = 1;
+        fade.GetComponent<Animator>().SetBool("Fade", true);
+        yield return new WaitUntil(() => fade.GetComponent<Image>().color.a == 1);
+
+        SceneManager.LoadScene(s);
+    }
+}
